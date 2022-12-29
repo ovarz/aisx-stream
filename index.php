ff6c007ce29fbe2b9232ad4f540566e1f3af12ea
ovarz <ovarz_03@yahoo.com>
2022-12-29 17:29:58 +0700

    bg

--- index.php
+++ index.php
@@ -12,27 +12,25 @@
     </span>
   </section>
   
-  
-  
-  <div class="bg-bottom"></div>
-  
-  
-  
-  <div class="float-player-bg"></div>
-  <section id="uniquePlayer-5" class="webPlayer light audioPlayer float-player">
-    <div id="uniqueContainer-5" class="videoPlayer"></div>
-    <div style="display:none;" class="playerData">
-      {
-        "name":"sonic",
-        "autoplay":"true",
-        "size":{"width":"510px"},
-        "media":{
-          "m4a":"http://175.103.48.4:9304/;"
-        }
-      }
-    </div>
-  </section>
-  
 </div>
+  
+  
+  
+<section id="uniquePlayer-5" class="webPlayer light audioPlayer float-player">
+  <div id="uniqueContainer-5" class="videoPlayer"></div>
+  <div style="display:none;" class="playerData">
+    {
+      "name":"Asix IDWS",
+      "autoplay":"true",
+      "size":{"width":"510px"},
+      "media":{
+        "m4a":"http://175.103.48.4:9304/;"
+      }
+    }
+  </div>
+</section>
+  
+  
+  
 <?php require ($_SERVER['ASIX_IDWS'].'inc/footer.php')?>
 <?php require ($_SERVER['ASIX_IDWS'].'inc/base-bottom.php')?>
