15115c78f8f99dd5fa079cec61e3d08f26525b6a
ovarz <ovarz_03@yahoo.com>
2022-12-30 06:33:59 +0700

    bg

--- index.php
+++ index.php
@@ -6,17 +6,49 @@
 <h1 class="hide"><?php echo $page; ?></h1>
 <div class="rancak-container">
 
-  <section class="section-container">
-    <span class="width-max">
-	  tes
-    </span>
+  <section title="Menu" class="main-box main-box-1">
+    <a aria-label="Link_Title" title="Link_Title" class="main-link" href="">
+      <div class="main-link-thumb flex_ori thumb-loading">
+        <img alt="Img_Title" class="lazyload" data-original="img/logo.png" />
+      </div>
+    </a>
+	<div class="main-place flex_ori thumb-loading">
+	  <img alt="Img_Title" class="lazyload" data-original="img/main-place-1.svg" />
+	</div>
+  </section>
+
+
+
+  <section title="Menu" class="main-box main-box-2">
+    <a aria-label="Link_Title" title="Link_Title" class="main-link" href="">
+      <div class="main-link-thumb flex_ori thumb-loading">
+        <img alt="Img_Title" class="lazyload" data-original="img/logo.png" />
+      </div>
+    </a>
+	<div class="main-place flex_ori thumb-loading">
+	  <img alt="Img_Title" class="lazyload" data-original="img/main-place-2.svg" />
+	</div>
+  </section>
+
+
+
+  <section title="Menu" class="main-box main-box-3">
+    <a aria-label="Link_Title" title="Link_Title" class="main-link" href="">
+      <div class="main-link-thumb flex_ori thumb-loading">
+        <img alt="Img_Title" class="lazyload" data-original="img/logo.png" />
+      </div>
+    </a>
+	<div class="main-place flex_ori thumb-loading">
+	  <img alt="Img_Title" class="lazyload" data-original="img/main-place-3.svg" />
+	</div>
   </section>
   
 </div>
   
   
-  
-<section id="uniquePlayer-5" class="webPlayer light audioPlayer float-player">
+
+<div class="bg-bottom"></div>
+<section title="Streaming" id="uniquePlayer-5" class="webPlayer light audioPlayer float-player">
   <div id="uniqueContainer-5" class="videoPlayer"></div>
   <div style="display:none;" class="playerData">
     {
