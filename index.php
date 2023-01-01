cc966a1d213e33904bd34d717b8199f0a88296bd
ovarz <ovarz_03@yahoo.com>
2023-01-01 13:09:52 +0700

    change bg (again)

--- index.php
+++ index.php
@@ -42,12 +42,24 @@
 	  <img alt="Img_Title" class="lazyload" data-original="img/main-place-3.svg" />
 	</div>
   </section>
+
+
+
+  <section title="Menu" class="main-box main-box-4">
+    <a aria-label="Link_Title" title="Link_Title" class="main-link" href="">
+      <div class="main-link-thumb flex_ori thumb-loading">
+        <img alt="Img_Title" class="lazyload" data-original="img/logo.png" />
+      </div>
+    </a>
+	<div class="main-place flex_ori thumb-loading">
+	  <img alt="Img_Title" class="lazyload" data-original="img/main-place-1.svg" />
+	</div>
+  </section>
   
 </div>
-  
-  
 
-<div class="bg-bottom"></div>
+
+
 <section title="Streaming" id="uniquePlayer-5" class="webPlayer light audioPlayer float-player">
   <div id="uniqueContainer-5" class="videoPlayer"></div>
   <div style="display:none;" class="playerData">
